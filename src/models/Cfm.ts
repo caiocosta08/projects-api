745cfa97fd1b1317b7fc2458fe012fbbbc685f32
caio <macbook@MacBook-Pro-de-Macbook.local>
2023-10-07 15:50:06 -0300

    3

--- src/models/Cfm.ts
+++ src/models/Cfm.ts
@@ -4,12 +4,18 @@ export interface ICfm extends Document {
     name: string;
     email: string;
     phone: string;
+    is_paid: boolean;
+    event: string;
+    date: string;
 }
 
 const cfmSchema: Schema = new Schema({
     name: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     phone: { type: String, required: true, unique: true },
+    is_paid: { type: Boolean, default: false},
+    event: { type: String, default: '' },
+    date: { type: String, default: new Date() },
 });
 
 export default mongoose.model<ICfm>('Cfm', cfmSchema);
